1a9bc5fd628451f486d60f66d3bbebab9f12e99f
adeelkhattak <adeelkhattak88088@gmail.com>
2026-02-09 23:27:21 +0500

    Testimonial Headings

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ScrollSmoother, ScrollToPlugin , ScrollTrigger } from 'gsap/all';
-import Hero from './sections/Hero'
+// import Hero from './sections/Hero'
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import Cars from './sections/Cars';
@@ -15,7 +15,7 @@ gsap.registerPlugin(ScrollTrigger , ScrollToPlugin ,  ScrollSmoother);
 const App = () => {
   useGSAP(()=>{
   ScrollSmoother.create({
-    smooth: 2,
+    smooth: 3,
     effects: true,
   } )
 }) 
@@ -26,7 +26,7 @@ const App = () => {
       <div id="smooth-wrapper">   
       <div id="smooth-content">
          
-<Hero/>
+{/* <Hero/> */}
 <Cars/>
 
 <div>
