599f4d9d2f9e0fc81a00e80cf18a8e81aadfcb8f
adeelkhattak <adeelkhattak88088@gmail.com>
2026-02-10 17:12:05 +0500

    Testimonial Headings done 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ScrollSmoother, ScrollToPlugin , ScrollTrigger } from 'gsap/all';
-// import Hero from './sections/Hero'
+import Hero from './sections/Hero'
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import Cars from './sections/Cars';
@@ -8,7 +8,7 @@ import Marketing from './sections/Marketing';
 import TechnologySection from './sections/Technology';
 import EngineeringSection from './sections/Engineering';
 import TestPage from './sections/Test';
-// import ModernTestimonialsSection from './sections/Testimonial';
+import ModernTestimonialsSection from './sections/Testimonial';
 
 gsap.registerPlugin(ScrollTrigger , ScrollToPlugin ,  ScrollSmoother);
 
@@ -26,7 +26,7 @@ const App = () => {
       <div id="smooth-wrapper">   
       <div id="smooth-content">
          
-{/* <Hero/> */}
+<Hero/>
 <Cars/>
 
 <div>
@@ -34,7 +34,7 @@ const App = () => {
 <TechnologySection/>
 <EngineeringSection/>
 <TestPage/>
-{/* <ModernTestimonialsSection/> */}
+<ModernTestimonialsSection/>
 
 </div>
     
